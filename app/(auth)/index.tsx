e4d3362caed0a9f359faec07f91e033eb158974e
MoustaqimSami <moustaqimsami1@gmail.com>
2026-02-10 19:54:59 -0700

    App now starts in signup-social

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -2,5 +2,5 @@ import { Redirect } from "expo-router";
 import React from "react";
 
 export default function AuthIndex() {
-  return <Redirect href="/(auth)/login" />;
+  return <Redirect href="/(auth)/signup-social" />;
 }

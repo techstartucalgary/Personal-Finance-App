b5034ae6ed52157aa6fe1edd270df9c703a4160f
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2026-03-11 13:52:14 -0600

    feat: Add ChatAI tab to navigation - Create chat screen with message displays and input - Integrate useChat hook for managing messages - Keep styling to minimum so frontend and designer can implement final UI without needing to remove conflicting styles

--- app/(tabs)/chatAI.tsx
+++ app/(tabs)/chatAI.tsx
@@ -1,9 +1,67 @@
+import { generateAPIUrl } from '@/utils/apiUrlGenerator';
+import { useChat } from '@ai-sdk/react';
+import { DefaultChatTransport } from 'ai';
+import { fetch as expoFetch } from 'expo/fetch';
+import { useState } from 'react';
+import { SafeAreaView, ScrollView, Text, TextInput, View } from 'react-native';
 
+export default function ChatAI() {
+  const [input, setInput] = useState('');
 
-export default function ChatAI(){
+  const apiUrl = generateAPIUrl('/api/chat');
+  console.log('API URL:', apiUrl);
 
+  const { messages, error, sendMessage } = useChat({
+    transport: new DefaultChatTransport({
+      fetch: expoFetch as unknown as typeof globalThis.fetch,
+      api: apiUrl,
+    }),
+    onError: error => console.error(error, 'ERROR'),
+  });
 
-    return(<div>
-        hello
-    </div>)
+  if (error) return <Text>{error.message}</Text>;
+
+  return (
+    <SafeAreaView style={{ height: '100%' }}>
+      <View
+        style={{
+          height: '95%',
+          display: 'flex',
+          flexDirection: 'column',
+          paddingHorizontal: 8,
+        }}
+      >
+        <ScrollView style={{ flex: 1 }}>
+          {messages.map(m => (
+            <View key={m.id} style={{ marginVertical: 8 }}>
+              <View>
+                <Text style={{ fontWeight: 700 }}>{m.role}</Text>
+                {m.parts.map((part, i) => {
+                  switch (part.type) {
+                    case 'text':
+                      return <Text key={`${m.id}-${i}`}>{part.text}</Text>;
+                  }
+                })}
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+
+        <View style={{ marginTop: 8 }}>
+          <TextInput
+            style={{ backgroundColor: 'white', padding: 8 }}
+            placeholder="Say something..."
+            value={input}
+            onChange={e => setInput(e.nativeEvent.text)}
+            onSubmitEditing={e => {
+              e.preventDefault();
+              sendMessage({ text: input });
+              setInput('');
+            }}
+            autoFocus={true}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
